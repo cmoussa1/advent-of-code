c17fcc19213253f057211842a7ed54fa090f0733
Christopher Moussa <moussa1@llnl.gov>
2020-04-22 14:35:18 -0700

    Initial commit
    Add day 1 of advent of code

---
+++ src/day-1/day1.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+
+    // total of all the modules
+    let mut total: i32 = 0;
+
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines("modules.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(mass) = line {
+                // cast the String mass to an int and perform the calculation
+                let val: i32 = (mass.parse::<i32>().unwrap() / 3) - 2;
+                total += val; // increment the total value with newly calculated val
+                println!("old value = {} ->  new value = {}", ip, val);
+            }
+        }
+    }
+
+    println!("The total is: {}", total);
+}
+
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
